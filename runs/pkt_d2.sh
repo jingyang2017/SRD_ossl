2e5da7f24b7c57271121f6bc3134358707b709ad
Jing Yang <jinang@fb.com>
2022-05-12 20:52:02 +0100

    increase weight

--- runs/pkt_d2.sh
+++
@@ -1,3 +0,0 @@
-python train_student.py --path_t ./save/models/wrn_40_2_vanilla/ckpt_epoch_240.pth  --model_s wrn_40_1 --distill pkt -a 0 -b 30000 --ood tin --v2
-python train_student.py --path_t ./save/models/resnet32x4_vanilla/ckpt_epoch_240.pth --model_s resnet8x4 --distill pkt -a 0 -b 30000 --ood tin --v2
-python train_student.py --path_t ./save/models/resnet32x4_vanilla/ckpt_epoch_240.pth --model_s ShuffleV1 --distill pkt -a 0 -b 30000 --ood tin --v2
